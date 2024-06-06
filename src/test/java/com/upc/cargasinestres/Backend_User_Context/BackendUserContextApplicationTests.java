7475f4c164c373f47f8fde7f085f47912acbfa43
Jorge Enrique Gonzales Carrión <104078975+JorgeGonzales15@users.noreply.github.com>
2024-06-05 20:21:13 -0500

    Initial commit

---
+++ src/test/java/com/upc/cargasinestres/Backend_User_Context/BackendUserContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.cargasinestres.Backend_User_Context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUserContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
